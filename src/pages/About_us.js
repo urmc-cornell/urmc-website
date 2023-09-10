1b4e99620e1fab6d893ae3c08aaf64a57db440ba
elvis <elvis@dhcp-vl2041-8882.eduroam.cornell.edu>
2023-09-10 17:27:24 -0400

    new changes

--- src/pages/About_us.js
+++ src/pages/About_us.js
@@ -10,35 +10,40 @@ import Widget from '../components/Widget';
 class About_us extends Component {
 
     render() {
-        return <about_us>
-            <div className="about-us">
-                <h1>About Us</h1>
-            </div>
-            <div className="about-us">
-                <h6 className="purpose"> The purpose of Underrepresented Minorities in Computing is to promote diversity
-                    within the computing fields and foster an environment that empowers underrepresented
-                    minorities with technological aspirations through <span style={{ color: '#265999' }}>3 pillars:</span></h6>
-            </div>
-            <div className="pillars">
-                <img src={academic} className="pillar"></img>
-                <img src={community} className="pillar"></img>
-                <img src={profdev} className="pillar"></img>
-            </div>
-
-
-            <div className="academic-support">
-                <div className="academic-top">
-                    <Carousel></Carousel>
-                    <AboutUsPillarCard title={Constants.AcademicCard.CardTitle} body={Constants.AcademicCard.CardBody}></AboutUsPillarCard>
+        return (
+            <about_us className="about-us-page">
+                <div>
+                    <div className="about-us">
+                        <h1>About Us</h1>
+                    </div>
+                    <div className="about-us">
+                        <h6 className="purpose"> The purpose of Underrepresented Minorities in Computing is to promote diversity
+                            within the computing fields and foster an environment that empowers underrepresented
+                            minorities with technological aspirations through <span style={{ color: '#265999' }}>3 pillars:</span></h6>
+                    </div>
                 </div>
-                <div className="widgets">
-                    <Widget></Widget>
-                    <Widget></Widget>
-                    <Widget></Widget>
-                    <Widget></Widget>
+
+                <div className="pillars">
+                    <img src={academic} className="pillar"></img>
+                    <img src={community} className="pillar"></img>
+                    <img src={profdev} className="pillar"></img>
                 </div>
-            </div>
-        </about_us>
+
+                <div className="academic-support">
+                    
+                    <div className="academic-top">
+                        <Carousel></Carousel>
+                        <AboutUsPillarCard title={Constants.AcademicCard.CardTitle} body={Constants.AcademicCard.CardBody}></AboutUsPillarCard>
+                    </div>
+                    <div className="widgets">
+                        {/* <Widget></Widget>
+                        <Widget></Widget>
+                        <Widget></Widget>
+                        <Widget></Widget> */}
+                    </div>
+                </div>
+            </about_us>
+        )
     }
 }
 
