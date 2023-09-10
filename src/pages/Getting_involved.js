1fedaa94b9fb01e8c17cebef9aa2645d8ff92fc4
Ronald Jabouin <14562+rj336@users.noreply.github.coecis.cornell.edu>
2023-09-10 13:18:51 -0400

    added links for listserv to buttons

--- src/pages/Getting_involved.js
+++ src/pages/Getting_involved.js
@@ -15,7 +15,7 @@ export default function Getting_involved() {
                             <h3 className='subheader-text'>Find out about URMC events, on-campus and off-campus opportunities, and other events happening within the CIS community!</h3>
                         </div>
                         <div className='button-div'> 
-                            <a class="btn btn-2" href="" target="_blank">JOIN THE LISTSERV</a>
+                            <a class="btn btn-2" href="https://docs.google.com/forms/d/e/1FAIpQLSc8c-drC3WBcVMqknx09q6-cBWRYeCbZYN1tBu98RM4CuDoTg/viewform" target="_blank">JOIN THE LISTSERV</a>
                         </div>
                     </div>
                     <div className='info-card'> 
@@ -33,7 +33,7 @@ export default function Getting_involved() {
                             <h3 className='subheader-text'>Are you an alumni who wants to keep up with URMC? Sign up to receive updates about URMC events and opportunities to get involved! </h3>
                         </div>
                         <div className='button-div'> 
-                            <a class="btn btn-2" href="" target="_blank">JOIN THE LISTSERV</a>
+                            <a class="btn btn-2" href="https://docs.google.com/forms/d/e/1FAIpQLSc8c-drC3WBcVMqknx09q6-cBWRYeCbZYN1tBu98RM4CuDoTg/viewform" target="_blank">JOIN THE LISTSERV</a>
                         </div>
                     </div>
                 </div>
