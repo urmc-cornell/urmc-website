ad6ef228d6f5e2da47724e5222c27e87793e5f57
Ronald Jabouin <14562+rj336@users.noreply.github.coecis.cornell.edu>
2023-09-10 20:12:38 -0400

    started corporate sponsors page

--- src/pages/Sponsors.js
+++ src/pages/Sponsors.js
@@ -1,5 +1,11 @@
+import '../styles/sponsors.css';
+
+
 export default function Sponsors() {
     return (
-        <h1>Under Construction!</h1>
+        <div className="heading"> 
+            <h1 className="corporate sponsors text"> 2023-2024 CORPORATE SPONSORS </h1>
+            <h3 className="thanks">Thank you for all your continued support of our organization!</h3>
+        </div>
     );
 }
