c668411c570bc38244433c80ed282f170bbb043d
Ronald Jabouin <14562+rj336@users.noreply.github.coecis.cornell.edu>
2023-09-12 11:51:52 -0400

    added buttons to corporate page

--- src/pages/Sponsors.js
+++ src/pages/Sponsors.js
@@ -1,11 +1,20 @@
 import '../styles/sponsors.css';
+import '../styles/home.css'
 
 
 export default function Sponsors() {
     return (
-        <div className="heading"> 
-            <h1 className="corporate sponsors text"> 2023-2024 CORPORATE SPONSORS </h1>
-            <h3 className="thanks">Thank you for all your continued support of our organization!</h3>
+        <div> 
+            <div className="heading"> 
+                <h1 className="corporate sponsors text"> 2023-2024 CORPORATE SPONSORS </h1>
+                <h3 className="thanks">Thank you for all your continued support of our organization!</h3>
+            </div>
+            <div className="buttons"> 
+                <a class="btn btn-2" href="" target="_blank">DONATE</a>
+                <a class="btn btn-1" href="" target="_blank">GETTING INVOLVED</a>
+
+            </div>
         </div>
+
     );
 }
