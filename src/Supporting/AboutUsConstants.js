23e21182a5adc39e792eaa4ca3203653e78bb0c5
elvis <elvis@elviss-Air.lan>
2023-09-14 02:18:24 -0400

    about us page wrapping up, both on web and mobile dev

---
+++ src/Supporting/AboutUsConstants.js
@@ -0,0 +1,104 @@
+import slackIcon from '../images/assets/mdi_slack.png'
+import examIcon from '../images/assets/mdi_pencil-box.png'
+import studyIcon from '../images/assets/mdi_book.png'
+import taIcon from '../images/assets/ph_chalkboard-teacher-fill.png'
+
+import handshakeIcon from '../images/assets/solid_handshake-angle.png'
+import gbodyIcon from '../images/assets/mdi_talking.png'
+import wifiIcon from '../images/assets/wifi_bar.png'
+import socialIcon from '../images/assets/movie-filter.png'
+
+import messageIcon from '../images/assets/ic_round-question-answer.png'
+import corporateIcon from '../images/assets/mdi_company.png'
+import codeIcon from '../images/assets/material-symbols_code-blocks.png'
+import resumeIcon from '../images/assets/mdi_resume.png'
+
+export const Academic = {
+    Card: {
+        title: "Ensuring academic success",
+        body: "Our wonderful academic chairs work to uphold and facilitate academic success. It is crucial that GBody members feel supported academically but more importantly, never feel alone."
+    },
+
+    Widget: {
+        Slack: {
+            icon: slackIcon,
+            title: "Peer to peer communication",
+            body: "Class channels on Slack"
+        },
+        Exam: {
+            icon: examIcon,
+            title: "Exam preparation",
+            body: "Prelim review sessions"
+        },
+        Study: {
+            icon: studyIcon,
+            title: "Study time & spaces",
+            body: "Weekly office hours"
+        },
+        TA: {
+            icon: taIcon,
+            title: "Teaching Assistants",
+            body: "Access to TA network & events"
+        }
+    }
+}
+
+export const Community = {
+    Card: {
+        title: "Building community",
+        body: "The family we have created sets us apart from other organizations and really helps our members to thrive. Outreach puts all of their efforts into strengthening the community and also attracting more people into this family, regardless of their major!"
+    },
+
+    Widget: {
+        Slack: {
+            icon: handshakeIcon,
+            title: "Mentorship",
+            body: "M&M Mentorship Program"
+        },
+        Exam: {
+            icon: gbodyIcon,
+            title: "General Body Meetings",
+            body: "Peer discussion and goals"
+        },
+        Study: {
+            icon: wifiIcon,
+            title: "Outreach",
+            body: "Freshman Under the Hood Series"
+        },
+        TA: {
+            icon: socialIcon,
+            title: "Social Outings",
+            body: "Movies, bowling, skating, etc. "
+        }
+    }
+}
+
+export const Professional = {
+    Card: {
+        title: "Promoting professional excellence",
+        body: "Our amazing professional development chairs work to promote professional excellence. Working to help our members grow at the very beginning of their professional careers, and supporting URMs as they explore their interests."
+    },
+
+    Widget: {
+        Slack: {
+            icon: messageIcon,
+            title: "Interview Preparation",
+            body: "Mock Interviews & questions"
+        },
+        Exam: {
+            icon: corporateIcon,
+            title: "Corporate Events",
+            body: "Company recruitment"
+        },
+        Study: {
+            icon: codeIcon,
+            title: "Workshops",
+            body: "Technical skills reviews"
+        },
+        TA: {
+            icon: resumeIcon,
+            title: "Resumes",
+            body: "Resume reviews & resume book"
+        }
+    }
+}
--- src/Supporting/StringConstants.js
+++
@@ -1,7 +0,0 @@
-export const AcademicSupportCardTitle = "Ensuring academic success";
-
-export const AcademicCard = {
-    CardTitle: "Ensuring academic success",
-    CardBody: "Our wonderful academic chairs work to uphold and facilitate academic success. It is crucial that GBody members feel supported academically but more importantly, never feel alone."
-}
-
