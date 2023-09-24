e64a788269fb1348ba93610d89094332f1561bdd
Ronald Jabouin <14562+rj336@users.noreply.github.coecis.cornell.edu>
2023-09-23 20:24:49 -0400

    corrected text on leadership page to be fall 2023

--- src/pages/Leadership.js
+++ src/pages/Leadership.js
@@ -13,16 +13,6 @@ class Leadership extends Component {
         };
     }
 
-
-    componentDidMount() {
-        fetch("https://script.googleusercontent.com/macros/echo?user_content_key=A957UGVKpaj3sDPBnmSFp4J-e8kVYycT_qVI9nzg9Jl4GpMr9Zue9vkWNRMS3VDrKDe39EiKGVmvpuHwsAobatNo28AVKCy4m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnMGN2GrXRN10VNZKmRWmvMUvmnimkcUT1nXOlb_Wu_f5FYze0Vr-3oU0m1PRW2sM3sU7zjgJ81MccfaD4rSjTHfwtV92wPHIJw&lib=MRUYj6yJa0sVkk_VgpuBrFp7oKR2YfmXG")
-        .then(response => response.json())
-        // .then(data => 
-        // );
-
-
-    }
-
     handleCardClick = (card) => {
         this.setState({ selectedCard: card, popupActive: card.popup });
     }
@@ -34,9 +24,9 @@ class Leadership extends Component {
     render() {
         const { selectedCard } = this.state;
 
-        return <div className="leadership-page">
+        return <div>
             <h1 className="leadership">Executive Board</h1>
-            <h2 className="fall22"> Spring 2023</h2>
+            <h2 className="fall22"> Fall 2023</h2>
             <EboardPopup trigger={this.state.popupActive} card={this.state.selectedCard} setTrigger={this.handleClose}>
             </EboardPopup>
             <div className="grid-container-container">
