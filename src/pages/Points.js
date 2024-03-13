97ec952900f4fe990fb5ed7a3ed6a5264fb9d9f1
Yoseph Endawoke <ye38@cornell.edu>
2024-03-13 09:20:06 -0400

    updated points page contains cut off issue

--- src/pages/Points.js
+++ src/pages/Points.js
@@ -1,6 +1,8 @@
 import '../styles/sponsors.css';
-import '../styles/home.css'
-
+import '../styles/home.css'; 
+import '../styles/Leadership.css'; 
+import '../styles/about_us.css'; 
+import '../styles/points.css'; 
 
 
 export default function Points() {
@@ -8,10 +10,42 @@ export default function Points() {
         <div> 
             <div className="heading"> 
                 <h1 className="Points"> Points Tracking </h1>
-                <h3 className="thanks">URMC Collects Points To Reward Active Members</h3>
+                <h3 className="subheader">URMC Collects Points To Reward Active Members</h3>
+                
                 {/* Point Breakdown */}
-                {/* Text field to check points */}
+                <div className="subheader">
+                    <h2 className="fall22">Earning Points</h2>
+                    <h3 className="thanks">Members can earn points by...</h3>
+                
+                    <div className="fall22">
+                        <ul>
+                            <li>Attending events and completing the attendance form.</li>
+                            <li>Participating in mentor matching as a mentor or mentee.</li>
+                            <li>and doing something else....</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div className="view_points">
+                    <h2 className="fall22">View Points</h2>
+                    <h3 className="subheader">Enter your netID below to see your points</h3>
+                    
+                    <div className="netID input">
+                        <form id="points-form" action="#">
+                            <input type="text" id="netID" name="netID" placeholder="Enter netID here"></input>
+                    
+                        </form>
+                    </div>
+                </div>
+
             </div>
+            
+
+
+                {/* Text field to check points */}
+
+            
+            
         </div>
     );
 }
