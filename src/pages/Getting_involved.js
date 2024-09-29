1aa755cc179d65235b74c1424fdca19326083f0a
yoyo3245 <157998034+yoyo3245@users.noreply.github.com>
2024-09-29 17:42:07 -0400

    fixed sponsors and alignment

--- src/pages/Getting_involved.js
+++ src/pages/Getting_involved.js
@@ -9,21 +9,21 @@ export default function Getting_involved() {
             <div className='grid-container'>
                     <div className=''> 
                         <div className="text-background">
-                            <h1 className='header-text'> STUDENTS </h1>
-                            <h3 className='subheader-text'>Find out about URMC events, on-campus and off-campus opportunities, and other events happening within the CIS community!</h3>
-                        </div>
-                        <div className='button-div'> 
-                        <a class="btn btn-1" href="https://join.slack.com/t/urmc/shared_invite/zt-2dy8ndtoy-~6zcRR2skt7Z5iT5iAyIBg" target="_blank">JOIN THE SLACK</a>
-
-                        </div>
-                    </div>
-                    <div className=''> 
-                        <div className="text-background">
                             <h1 className='header-text'> COMPANIES </h1>
                             <h3 className='subheader-text'>Send us an email to connect with our Corporate directors and receive our Sponsorship Packet! You won't regret it!</h3>
                         </div>
                         <div className='button-div'> 
                             <a class="btn btn-1" href="mailto:urmc@cornell.edu" target="_blank">SPONSOR US</a> 
+                        </div>
+                    </div>
+                    <div className=''> 
+                        <div className="text-background">
+                            <h1 className='header-text'> STUDENTS </h1>
+                            <h3 className='subheader-text'>Find out about URMC events, on-campus and off-campus opportunities, and other events happening within the CIS community!</h3>
+                        </div>
+                        <div className='button-div'> 
+                            <a class="btn btn-1" href="https://join.slack.com/t/urmc/shared_invite/zt-2dy8ndtoy-~6zcRR2skt7Z5iT5iAyIBg" target="_blank">JOIN THE SLACK</a>
+
                         </div>
                     </div>
                     <div className=''> 
--- src/pages/Sponsors.js
+++ src/pages/Sponsors.js
@@ -38,11 +38,11 @@ export default function Sponsors() {
                 <div className="sponsor-section silver-sponsors"> 
                     <a className="btn-small btn-silver">SILVER</a>
                         {/* <img src={meta} alt="Meta Logo" className="logo"></img> */}
-                        <img src={duolingo} alt="duolingo Logo" className="logo"></img>
+                        <img src={duolingo} alt="duolingo Logo" className=" duolingo"></img>
                         {/* <img src={goldman} alt="goldman Logo" className="logo"></img> */}
                         {/* <img src={yext} alt="yext Logo" className="logo"></img> */}
                         {/* <img src={oracle} alt="oracle Logo" className="logo"></img> */}
-                        <img src={capitalone} alt="Capital One Logo" className="logo"></img>
+                        <img src={capitalone} alt="Capital One Logo" className="duolingo"></img>
                         <img src={bloomberg} alt="Bloomberg Logo" className="logo bloomberg"></img>
 
                 </div>
