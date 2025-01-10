fa57f7d75dedb235174e9f2abe4e1961715e11a1
Temi Adebowale <ta375@cornell.edu>
2025-01-10 13:31:56 -0600

    Small formatting changes and adding comments

--- src/pages/Leadership.js
+++ src/pages/Leadership.js
@@ -1,129 +1,143 @@
-import React, { Component } from 'react'
-import EboardCard from '../components/EboardCard';
-import '../styles/Leadership.css';
-import EboardPopup from '../components/eboardPopup';
-import { cards } from '../Supporting/Leadership-Constants.js'
+import React, { Component } from "react";
+import EboardCard from "../components/EboardCard";
+import "../styles/Leadership.css";
+import EboardPopup from "../components/eboardPopup";
+import { cards } from "../Supporting/Leadership-Constants.js";
 
-
-
-let sheetID = "1MSvTcAeph8ehYwtBIBBbcLLkVg3JhWwtTCz1ODSEl1w"
-let sheetTitle = "eboard"
+let sheetID = "1MSvTcAeph8ehYwtBIBBbcLLkVg3JhWwtTCz1ODSEl1w";
+let sheetTitle = "eboard";
 //SHEET_RANGE might have to be changed if we ever have >30 events, but probably not
-let sheetRange = 'A2:O37'
-let fullURL = ("https://docs.google.com/spreadsheets/d/" + sheetID + '/gviz/tq?sheet=' + sheetTitle + '&range=' + sheetRange);
+let sheetRange = "A2:O37";
+let fullURL =
+  "https://docs.google.com/spreadsheets/d/" +
+  sheetID +
+  "/gviz/tq?sheet=" +
+  sheetTitle +
+  "&range=" +
+  sheetRange;
 
 class Leadership extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedCard: null,
-            popupActive: false,
-            eboard_list: [],
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedCard: null,
+      popupActive: false,
+      eboard_list: [],
+    };
+  }
 
-    componentDidMount() {
-        fetch(fullURL)
-            .then(res => res.text())
-            .then(rep => {
-                // figure out why 47, 0, -2 here 
-                    // its from some tutorial but it basicially gets you each row 
-                    // and it's information which we use below
-                
-                let data = JSON.parse(rep.substring(47).slice(0, -2));
-                let activeRows = data.table.rows.length;
+  // Data is fetched when component mounts
 
-                let eboard = []
+  componentDidMount() {
+    fetch(fullURL)
+      .then((res) => res.text())
+      .then((rep) => {
+        // figure out why 47, 0, -2 here
+        // its from some tutorial but it basicially gets you each row
+        // and it's information which we use below
 
-                for (let i = 0; i < activeRows; i++) {
-                    let id = data.table.rows[i].c[0].v
-                    let title = data.table.rows[i].c[4].v
-                    let headshot_one = data.table.rows[i].c[5].v
-                    let name = data.table.rows[i].c[2].v
-                    let headshot_two = data.table.rows[i].c[6].v
-                    let majors = data.table.rows[i].c[9].v
-                    let instagram =  data.table.rows[i].c[10].v
-                    let linkedin = data.table.rows[i].c[11].v
-                    let askAbout = data.table.rows[i].c[8].v
-                    let bio = data.table.rows[i].c[7].v
-                    var popup = true
+        let data = JSON.parse(rep.substring(47).slice(0, -2));
+        let activeRows = data.table.rows.length;
 
+        let eboard = [];
 
-                    let headshot_one_id = headshot_one.split('/')[5];
-                    let headshot_one_url = "https://drive.google.com/uc?id=" + headshot_one_id
-                    // https://drive.google.com/uc?id=
+        // Loop through each row and create eboard cards
 
-                    let headshot_two_id = headshot_two.split('/')[5];
-                    let headshot_two_url = "https://drive.google.com/uc?id=" + headshot_two_id
+        for (let i = 0; i < activeRows; i++) {
+          let id = data.table.rows[i].c[0].v;
+          let title = data.table.rows[i].c[4].v;
+          let headshot_one = data.table.rows[i].c[5].v;
+          let name = data.table.rows[i].c[2].v;
+          let headshot_two = data.table.rows[i].c[6].v;
+          let majors = data.table.rows[i].c[9].v;
+          let instagram = data.table.rows[i].c[10].v;
+          let linkedin = data.table.rows[i].c[11].v;
+          let askAbout = data.table.rows[i].c[8].v;
+          let bio = data.table.rows[i].c[7].v;
+          var popup = true;
 
-                    if (bio != 'test') {
-                        popup = true
-                    } else {
-                        popup = false
-                    }
+          // Process Google Drive image URLs
+          let headshot_one_id = headshot_one.split("/")[5];
+          let headshot_one_url =
+            "https://drive.google.com/uc?id=" + headshot_one_id;
+          let headshot_two_id = headshot_two.split("/")[5];
+          let headshot_two_url =
+            "https://drive.google.com/uc?id=" + headshot_two_id;
 
-                    let eboard_card = {
-                        id: id,
-                        title: title,
-                        image: headshot_one_url,
-                        name: name,
-                        secondaryImage: headshot_two_url,
-                        majors: majors,
-                        insta: instagram,
-                        linkedIn: linkedin,
-                        // makes comma seperated list of interests
-                        askAbout: askAbout.split(","),
-                        bio: bio,
-                        // popup, popup
-                        popup: popup,
+          if (bio != "test") {
+            popup = true;
+          } else {
+            popup = false;
+          }
 
+          // Create eboard card object
 
-                    }
-                    eboard.push(eboard_card)
-                }
+          let eboard_card = {
+            id: id,
+            title: title,
+            image: headshot_one_url,
+            name: name,
+            secondaryImage: headshot_two_url,
+            majors: majors,
+            insta: instagram,
+            linkedIn: linkedin,
+            // makes comma seperated list of interests
+            askAbout: askAbout.split(","),
+            bio: bio,
+            // popup, popup
+            popup: popup,
+          };
+          eboard.push(eboard_card);
+        }
 
-                this.setState({
-                    eboard_list: eboard,
-                })
-            })
+        // Update state with eboard data
 
-    }
-    handleCardClick = (card) => {
-        this.setState({ selectedCard: card, popupActive: card.popup });
-    }
+        this.setState({
+          eboard_list: eboard,
+        });
+      });
+  }
+  handleCardClick = (card) => {
+    this.setState({ selectedCard: card, popupActive: card.popup });
+  };
 
-    handleClose = (active) => {
-        this.setState({ selectedCard: null, popupActive: active });
-    }
+  handleClose = (active) => {
+    this.setState({ selectedCard: null, popupActive: active });
+  };
 
+  render() {
+    const { selectedCard, eboard_list } = this.state;
 
-    render() {
-        const {selectedCard, eboard_list } = this.state;
-
-        return <div>
-            <h1 className="leadership">Executive Board</h1>
-            <h2 className="fall22"> Fall 2024</h2>
-            <EboardPopup trigger={this.state.popupActive} card={this.state.selectedCard} setTrigger={this.handleClose}>
-            </EboardPopup>
-            <div className="grid-container-container">
-                <div className="grid-container">
-                    {cards.map(card => (
-                        <div className="grid-item"
-                            onClick={() => this.handleCardClick(card)}
-                        >
-                            <EboardCard
-                                key={card.id}
-                                imageURL={card.image}
-                                title={card.title}
-                                name={card.name}
-                                onClick={() => this.handleCardClick(card)}
-                            />
-                        </div>
-                    ))}
-                </div>
-            </div>
+    return (
+      <div>
+        <h1 className="leadership">Executive Board</h1>
+        <h2 className="fall22"> Fall 2024</h2>
+        <EboardPopup
+          trigger={this.state.popupActive}
+          card={this.state.selectedCard}
+          setTrigger={this.handleClose}
+        ></EboardPopup>
+        <div className="grid-container-container">
+          <div className="grid-container">
+            {cards.map((card) => (
+              <div
+                className="grid-item"
+                onClick={() => this.handleCardClick(card)}
+              >
+                <EboardCard
+                  key={card.id}
+                  imageURL={card.image}
+                  title={card.title}
+                  name={card.name}
+                  onClick={() => this.handleCardClick(card)}
+                />
+              </div>
+            ))}
+          </div>
         </div>
-    }
+      </div>
+    );
+  }
 }
 
 export default Leadership;
--- src/pages/Points.js
+++ src/pages/Points.js
@@ -1,177 +1,208 @@
-import '../styles/sponsors.css';
-import '../styles/home.css'; 
-import '../styles/Leadership.css'; 
-import '../styles/about_us.css'; 
-import '../styles/points.css';
+import "../styles/sponsors.css";
+import "../styles/home.css";
+import "../styles/Leadership.css";
+import "../styles/about_us.css";
+import "../styles/points.css";
 
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
-let sheetID = "1Gmj6r89FF-QDD4NbBRG7nXKXjcQGUZbO6ddVM359H6k"
-let sheetTitle = "Points"
-//SHEET_RANGE might have to be changed if we ever have >30 events, but probably not
-let sheetRange = 'A2:C200'
-let fullURL = ("https://docs.google.com/spreadsheets/d/" + sheetID + '/gviz/tq?sheet=' + sheetTitle + '&range=' + sheetRange);
+// Google Sheets Configuration
+let sheetID = "1Gmj6r89FF-QDD4NbBRG7nXKXjcQGUZbO6ddVM359H6k";
+let sheetTitle = "Points";
+let sheetRange = "A2:C200"; //SHEET_RANGE might have to be changed if we ever have >30 events, but probably not
+let fullURL =
+  "https://docs.google.com/spreadsheets/d/" +
+  sheetID +
+  "/gviz/tq?sheet=" +
+  sheetTitle +
+  "&range=" +
+  sheetRange;
 
 export default function Points() {
+  // State for individual points lookup and leaderboard
+  const [points, setPoints] = useState(null);
+  const [leaderboard, setLeaderboard] = useState([]);
 
-    const [points, setPoints] = useState(null);
-    const [leaderboard, setLeaderboard] = useState([]);
+  // Function to fetch points for a specific netID
+  const fetchPoints = async (netid) => {
+    try {
+      const response = await fetch(`http://localhost:5000/api/points/${netid}`);
+      const data = await response.json();
+      setPoints(`${data.total_points} Points`);
+    } catch (error) {
+      console.error("Error fetching points:", error);
+      setPoints("Error fetching points");
+    }
+  };
 
-    // Function to fetch points for a specific netID
-    const fetchPoints = async (netid) => {
-        try {
-            const response = await fetch(`http://localhost:5000/api/points/${netid}`);
-            const data = await response.json();
-            setPoints(`${data.total_points} Points`);
-        } catch (error) {
-            console.error('Error fetching points:', error);
-            setPoints('Error fetching points');
+  // Fetch leaderboard data from Google Sheets on component mount
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(fullURL);
+        const data = await response.text();
+        const jsonData = JSON.parse(data.substring(47).slice(0, -2));
+
+        let board = [];
+        for (let i = 0; i < 10; i++) {
+          var str =
+            jsonData["table"]["rows"][i]["c"][1]["v"].toLowerCase() +
+            ": " +
+            jsonData["table"]["rows"][i]["c"][2]["f"] +
+            " Points";
+          board.push(str);
         }
-    };
+        setLeaderboard(board);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+    fetchData();
+  }, []); // Empty dependency array since we only want to fetch once on mount
 
-    // Fetch leaderboard on component mount
-    useEffect(() => {
-        async function fetchData() {
-          try {
-            const response = await fetch(fullURL);
-            const data = await response.text();
-            const jsonData = JSON.parse(data.substring(47).slice(0, -2));
+  // Handle individual points lookup from Google Sheets
+  const point_set = async (event) => {
+    event.preventDefault(); // Do not delete this line, stops page from refreshing
+    const netid = document.getElementById("netID").value;
+    var found = false;
 
-            let board = [];
-            for (let i = 0; i < 10; i++) {
-              var str = jsonData["table"]["rows"][i]["c"][1]["v"].toLowerCase() + ": " + jsonData["table"]["rows"][i]["c"][2]["f"] + " Points";
-              board.push(str);
-            }
-            setLeaderboard(board)
-  
-          } catch (error) {
-            console.error('Error:', error);
-          } 
-        }
-        fetchData();
-      }, []); // Empty dependency array since we only want to fetch once on mount
+    try {
+      const response = await fetch(fullURL);
+      const data = await response.text();
+      const jsonData = JSON.parse(data.substring(47).slice(0, -2));
 
-
-      const point_set = async (event) => {
-        event.preventDefault(); // Do not delete this line, stops page from refreshing
-        const netid = document.getElementById("netID").value;
-        var found = false;
-  
-        try {
-          const response = await fetch(fullURL);
-          const data = await response.text();
-          const jsonData = JSON.parse(data.substring(47).slice(0, -2));
-          
-          for (let i = 0; i < jsonData["table"]["rows"].length; i++) {
-            if (jsonData["table"]["rows"][i]["c"][1]["v"].toLowerCase() === netid.toLowerCase()) {
-              found = true;
-              setPoints("Points: " + jsonData["table"]["rows"][i]["c"][2]["f"]);
-              break;
-            }
-          }
-          if (!found) {
-            setPoints("NetID Not Found");
-          }
-        } catch (error) {
-          console.error('Error:', error);
-          setPoints("Error fetching points");
+      for (let i = 0; i < jsonData["table"]["rows"].length; i++) {
+        if (
+          jsonData["table"]["rows"][i]["c"][1]["v"].toLowerCase() ===
+          netid.toLowerCase()
+        ) {
+          found = true;
+          setPoints("Points: " + jsonData["table"]["rows"][i]["c"][2]["f"]);
+          break;
         }
       }
+      if (!found) {
+        setPoints("NetID Not Found");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      setPoints("Error fetching points");
+    }
+  };
 
-    return (
-        <div style={{ height: 1000, overflow: 'auto' }} className="points-page">
-            <div className="heading"> 
-                <h1 className="Points"> Points Tracking </h1>
-                <h3 className="subheader">URMC Collects Points To Reward Active Members</h3>
-                
-                {/* Main content container */}
-                <div className="points-container">
-                    {/* Left side - Points Breakdown */}
-                    <div className="points-section">
-                        <h2 className="fall22">Earning Points</h2>
-                        <h3 className="thanks">Members can earn points by...</h3>
-                    
-                        <div className="points-boxes">
-                            <div className="points-box green">
-                                <span className="points">3 Points</span>
-                                <span className="description">Attending events and completing the attendance form</span>
-                            </div>
-                            <div className="points-box orange">
-                                <span className="points">2 Points</span>
-                                <span className="description">Attending a non G-Body event</span>
-                            </div>
-                            <div className="points-box yellow">
-                                <span className="points">1 Point</span>
-                                <span className="description">Responding to URMC forms</span>
-                            </div>
-                        </div>
+  return (
+    <div style={{ height: 1000, overflow: "auto" }} className="points-page">
+      <div className="heading">
+        <h1 className="Points"> Points Tracking </h1>
+        <h3 className="subheader">
+          URMC Collects Points To Reward Active Members
+        </h3>
 
-                        {/* Points lookup form */}
-                        <div className="view_points">
-                            <h2 className="fall22">View Points</h2>
-                            <h3 className="subheader">Enter your netID below to see your points</h3>
-                            
-                            <div className="netID input">
-                                <form id="points-form" style={{display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '15px'}}>
-                                    <input 
-                                        type="text" 
-                                        id="netID" 
-                                        name="netID" 
-                                        placeholder="Enter netID here"
-                                        style={{
-                                            padding: '12px 20px',
-                                            fontSize: '16px',
-                                            borderRadius: '25px',
-                                            border: '2px solid #e0e0e0',
-                                            width: '250px',
-                                            outline: 'none',
-                                            transition: 'border-color 0.3s ease'
-                                        }}
-                                    />  
-                                    <button 
-                                        type="submit" 
-                                        id="myButton" 
-                                        onClick={point_set}
-                                        style={{
-                                            padding: '12px 30px',
-                                            fontSize: '16px',
-                                            borderRadius: '25px',
-                                            border: 'none',
-                                            backgroundColor: '#4CAF50',
-                                            color: 'white',
-                                            cursor: 'pointer',
-                                            transition: 'background-color 0.3s ease',
-                                            boxShadow: '0 2px 4px rgba(0,0,0,0.1)'
-                                        }}
-                                    >
-                                        Get Points
-                                    </button>
-                                    <h3 className="subheader">{points}</h3>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
+        {/* Main content container */}
+        <div className="points-container">
+          {/* Left side - Points Breakdown */}
+          <div className="points-section">
+            <h2 className="fall22">Earning Points</h2>
+            <h3 className="thanks">Members can earn points by...</h3>
 
-                    {/* Right side - Leaderboard */}
-                    <div className="leaderboard-section">
-                        <h2>Leaderboard</h2>
-                        <div className="fall22">
-                            <ul className="subheader" style={{textAlign: 'left', paddingLeft: '20px'}}>
-                                <li>{leaderboard[0]}</li>
-                                <li>{leaderboard[1]}</li>
-                                <li>{leaderboard[2]}</li>
-                                <li>{leaderboard[3]}</li>
-                                <li>{leaderboard[4]}</li>
-                                <li>{leaderboard[5]}</li>
-                                <li>{leaderboard[6]}</li>
-                                <li>{leaderboard[7]}</li>
-                                <li>{leaderboard[8]}</li>
-                                <li>{leaderboard[9]}</li>
-                            </ul> 
-                        </div>
-                    </div>
-                </div>
+            <div className="points-boxes">
+              <div className="points-box green">
+                <span className="points">3 Points</span>
+                <span className="description">
+                  Attending events and completing the attendance form
+                </span>
+              </div>
+              <div className="points-box orange">
+                <span className="points">2 Points</span>
+                <span className="description">
+                  Attending a non G-Body event
+                </span>
+              </div>
+              <div className="points-box yellow">
+                <span className="points">1 Point</span>
+                <span className="description">Responding to URMC forms</span>
+              </div>
             </div>
+
+            {/* Points lookup form */}
+            <div className="view_points">
+              <h2 className="fall22">View Points</h2>
+              <h3 className="subheader">
+                Enter your netID below to see your points
+              </h3>
+
+              <div className="netID input">
+                <form
+                  id="points-form"
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    gap: "15px",
+                  }}
+                >
+                  <input
+                    type="text"
+                    id="netID"
+                    name="netID"
+                    placeholder="Enter netID here"
+                    style={{
+                      padding: "12px 20px",
+                      fontSize: "16px",
+                      borderRadius: "25px",
+                      border: "2px solid #e0e0e0",
+                      width: "250px",
+                      outline: "none",
+                      transition: "border-color 0.3s ease",
+                    }}
+                  />
+                  <button
+                    type="submit"
+                    id="myButton"
+                    onClick={point_set}
+                    style={{
+                      padding: "12px 30px",
+                      fontSize: "16px",
+                      borderRadius: "25px",
+                      border: "none",
+                      backgroundColor: "#4CAF50",
+                      color: "white",
+                      cursor: "pointer",
+                      transition: "background-color 0.3s ease",
+                      boxShadow: "0 2px 4px rgba(0,0,0,0.1)",
+                    }}
+                  >
+                    Get Points
+                  </button>
+                  <h3 className="subheader">{points}</h3>
+                </form>
+              </div>
+            </div>
+          </div>
+
+          {/* Right side - Leaderboard */}
+          <div className="leaderboard-section">
+            <h2>Leaderboard</h2>
+            <div className="fall22">
+              <ul
+                className="subheader"
+                style={{ textAlign: "left", paddingLeft: "20px" }}
+              >
+                <li>{leaderboard[0]}</li>
+                <li>{leaderboard[1]}</li>
+                <li>{leaderboard[2]}</li>
+                <li>{leaderboard[3]}</li>
+                <li>{leaderboard[4]}</li>
+                <li>{leaderboard[5]}</li>
+                <li>{leaderboard[6]}</li>
+                <li>{leaderboard[7]}</li>
+                <li>{leaderboard[8]}</li>
+                <li>{leaderboard[9]}</li>
+              </ul>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
