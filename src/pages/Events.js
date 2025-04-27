1b0b9762b4027070863083e71abcd70a1fb800cb
yosephendawoke <ye38@cornell.edu>
2025-04-27 17:00:01 -0400

    fixed events frontend events

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -119,7 +119,7 @@ export default function Events() {
                 className="grid-item"
                 onClick={() => handleCardClick(event)}
               >
-                <iframe
+                <img
                   src={event.flyer}
                   width="100%"
                   height="100%"
