b46121cf9af5fbe26d5d8cf64efc6afcf9b91944
Fadi Ismail <fmi4@cornell.edu>
2026-04-12 21:55:48 -0400

    feat: rewrite Home.js with new Figma sections

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,15 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// react-slick requires matchMedia — polyfill for JSDOM test environment
+window.matchMedia = window.matchMedia || function () {
+  return {
+    matches: false,
+    addListener: () => {},
+    removeListener: () => {},
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => {},
+  };
+};
